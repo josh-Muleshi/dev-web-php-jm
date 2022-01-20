3a5d83fc0cfb2febeab84cb4ea56a4c222930272
josh-Muleshi <jmuleshi2@gmail.com>
2022-01-20 21:55:03 +0200

    add a new file call index.php in which i'm making forms

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <form method="post" action="#">
+        <table>
+            <tr>
+                <td>Nom</td>
+                <td><input type="text" name="nom" placeholder="muteba"></td>
+            </tr>
+            <tr>
+                <td>Postnom</td>
+                <td><input type="text" name="postnom" placeholder="muleshi"></td>
+            </tr>
+            <tr>
+                <td>Prenom</td>
+                <td><input type="text" name="prenom" placeholder="josh"></td>
+            </tr>
+            <tr>
+                <td>Matricule</td>
+                <td><input type="text" name="matricule" placeholder="22mm001"></td>
+            </tr>
+            <tr>
+                <td>Genre</td>
+                <td><input type="radio" name="homme" value="M" checked> M
+                <input type="radio" name="femme" value="F"> F</td>
+            </tr>
+            <tr>
+                <td>Promotion</td>
+                <td><input type="text" name="promotion" placeholder="l2gl"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email" placeholder="joshm@gmail.com"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="button" name="envoyer" value="Envoyer"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
+
