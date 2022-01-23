8695a8f78116ca9b94a06d1d2429bd019cb49c56
josh-Muleshi <jmuleshi2@gmail.com>
2022-01-23 22:13:45 +0200

    done my tp: verifie et show form's informations

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,10 +1,13 @@
 <?php
 
-    if (isset($_POST['name'])) {
+    if (isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['firstname'])) {
         $name = $_POST['name'];
         $lastname = $_POST['lastname'];
         $firstname = $_POST['firstname'];
-        echo "Bienvenu $name $lastname $firstname";
+        $matricule = $_POST['matricule'];
+        $promotion = $_POST['promotion'];
+        $email = $_POST['email'];
+        echo "Bienvenu $name $lastname $firstname $matricule $promotion $email";
     }else{
         echo 'Desole cette clef n\'existe pas';
     }
