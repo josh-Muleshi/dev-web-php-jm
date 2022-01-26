acc907907e8ba56bee029a5f2a9162f63f1477d0
josh-Muleshi <jmuleshi2@gmail.com>
2022-01-26 10:35:05 +0200

    condition, loops et session's practics

---
+++ seance-26-01-22/action.php
@@ -0,0 +1,42 @@
+<?php
+
+    session_start();
+
+    $articles = array(
+        ['pain', 1000],
+        ['chocolat', 5000],
+        ['yes', 4500],
+        ['mangue', 10000],
+        ['chips', 25000]
+    );
+
+    $art = $_POST['dsg'];
+    $qte = $_POST['quantite'];
+
+    foreach($articles as $a){
+        if ($art == $a[0]) {
+            $pu = $a[1];
+            break;
+        }
+    }
+
+    if ($qte > 0) {
+        $total = $qte * $pu;
+
+        $_SESSION['articles'][] = [$art, $qte, $pu, $total];
+
+        foreach($_SESSION['articles'] as $a){
+            echo 'Article : ' .$a[0]. '<br>';
+            echo 'Qte : ' .$a[1]. '<br>';
+            echo 'PU : ' .$a[2]. '<br>';
+            echo 'Total : ' .$a[3]. 'Fc<br>';
+            echo '<br><hr>';
+        }
+
+        
+
+    }else{
+        echo '<h3 style="color:red">Qte invalide</h3>';
+    }
+
+    //echo $qte > 0 ? "Total : ". $qte * $pu ." Fc" : '<h3 style="color:red">Qte invalide</h3>';
---
+++ seance-26-01-22/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratique du 26/01/22</title>
+</head>
+<body>
+        <form action="action.php" method="post">
+            Article : <select name="dsg" id="">
+                <option value="pain">Pain</option>
+                <option value="chocolat">Chocolat</option>
+                <option value="yes">Yes</option>
+                <option value="mangue">Mangue</option>
+                <option value="chips">Chips</option>
+            </select><br>
+            Qte : <input type="number" name="quantite" id=""><br>
+            <input type="submit" value="Envoyer">
+        </form>
+</body>
+</html>
